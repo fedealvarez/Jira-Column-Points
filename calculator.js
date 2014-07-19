94d3ab3596c809f21ad1cfcd29ad09860156d9fd
Jeremias Menichelli <jmenichelli@gmail.com>
2014-07-19 10:14:31 -0300

    Switch to native JavaScript
    Switch to native JavaScript. Closure added to prevent from collapsed functions on return.

--- calculator.js
+++ calculator.js
@@ -1,39 +1,47 @@
-var headers = $(".ghx-column-headers .ghx-column");
-var cols = $('.ghx-column.ui-sortable');
-var results = [];
-var initialized = false;
-var initialize = function() {
-	for (var i = 0; i < headers.length; i++) {
-		headers[i].children[0].innerHTML += "<div></div>";
-	}
-	initialized = true;
-	calculate();
-};
-var calculate = function() {
-	for (var i = 0; i < headers.length; i++) {
-	    var col_points = 0;
-	    var curr_col = cols[i];
-	    var issues = $(curr_col).find('div.ghx-issue');
-	    for (var j = 0; j < issues.length; j++) {
-	      var curr_issue = issues[j];
-	      var issue_points = parseFloat($(curr_issue).find('span.aui-badge').html());
-	      if (issue_points && !isNaN(issue_points)) {
-	          col_points += issue_points;
-	      }
-	    }
-	    results.push({"name": headers[i].children[0].innerHTML,
-	     "points" : col_points});
-		headers[i].children[0].children[0].innerHTML = "<b>(" + col_points + ")</b>";
-	    /*console.log(headers[i].children[0].innerHTML +
-	     ": " + col_points)*/
-	};
-	console.log('Done!');
-}
+var jiraPoints = (function(document){
+    var headers = document.querySelectorAll('.ghx-column-headers .ghx-column');
+    var cols = document.querySelectorAll('.ghx-column.ui-sortable');
 
-if(!initialized) {
-	initialize();
-}
-else {
-	calculate();
-}
-//Type calculate() on console to execute it on demand
+    var results = [];
+    var initialized = false;
+    var initialize = function() {
+        for (var i = 0; i < headers.length; i++) {
+            headers[i].querySelector('h2').innerHTML += "<div></div>";
+
+        };
+        initialized = true;
+        calculate();
+    };
+    var calculate = function() {
+        for (var i = 0; i < headers.length; i++) {
+            var col_points = 0;
+            var curr_col = cols[i];
+            var issues = curr_col.querySelectorAll('.ghx-issue');
+
+            for (var j = 0; j < issues.length; j++) {
+              var curr_issue = issues[j];
+              var issue_points = parseFloat(curr_issue.querySelector('span.aui-badge').innerHTML);
+
+              if (issue_points && !isNaN(issue_points)) {
+                  col_points += issue_points;
+              }
+            }
+            results.push({"name": headers[i].querySelector('h2').innerHTML,"points" : col_points});
+            headers[i].querySelector('h2 div').innerHTML = "<b>(" + col_points + ")</b>";
+
+        };
+        console.log('Done!');
+    }
+
+    if(!initialized) {
+        initialize();
+    }
+    else {
+        calculate();
+    }
+
+    return {
+        update : calculate
+    }
+    //Type jiraPoints.update() refresh
+})(document);

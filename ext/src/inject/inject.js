3b3b69e07d6e0d4f1cf15aea9d551a1af3825e50
federico alvarez <federico.alvarez@ordergroove.com>
2015-07-23 12:29:36 -0300

    Updated header points positioning

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,38 +1,39 @@
 var jiraPoints = function(){
     try {
-    	var headers = document.querySelectorAll('.ghx-column-headers .ghx-column'),
-        	cols = document.querySelectorAll('.ghx-columns')[0].childNodes,
-        	results = [],
-        	total_points = 0,
-        	board_name = document.getElementById('ghx-board-name');
+        var headers = document.querySelectorAll('.ghx-column-headers .ghx-column'),
+            cols = document.querySelectorAll('.ghx-columns')[0].childNodes,
+            results = [],
+            total_points = 0,
+            board_header = document.getElementById('ghx-view-selector'),
+            board_name = board_header.getElementsByTagName('h1')[0];
 
         if(!document.getElementById('jcp-board-points')) {
-        	board_name.innerHTML += '<div id="jcp-board-points" style="background: #cdcdcd; border-radius: 10px; font-size: 12px; min-width: 20px; display: inline; padding: 0 2px; font-weight: bold; margin: 6px; position: absolute; text-align: center"></div>';
+            board_name.innerHTML += '<span id="jcp-board-points" style="background: #cdcdcd; border-radius: 10px; font-size: 12px; min-width: 20px; display: inline; padding: 0 2px; font-weight: bold; margin: 9px 0; position: absolute; text-align: center"></span>';
         }
 
-	    for (var i = 0; i < headers.length; i++) {
-	        headers[i].querySelector('h2').innerHTML += '<div style="background: #cdcdcd; border-radius: 10px; font-size: 12px; min-width: 20px; text-align: center; position: absolute; bottom: 5px; right: 5px; padding: 0 2px;"></div>';
-	    }
+        for (var i = 0; i < headers.length; i++) {
+            headers[i].querySelector('h2').innerHTML += '<div style="background: #cdcdcd; border-radius: 10px; font-size: 12px; min-width: 20px; text-align: center; position: absolute; bottom: 5px; right: 5px; padding: 0 2px;"></div>';
+        }
 
-	    for (var i = 0; i < headers.length; i++) {
-	        var col_points = 0,
-	            curr_col = cols[i],
-	            issues = curr_col.querySelectorAll('.ghx-issue');
+        for (var i = 0; i < headers.length; i++) {
+            var col_points = 0,
+                curr_col = cols[i],
+                issues = curr_col.querySelectorAll('.ghx-issue');
 
-	        for (var j = 0; j < issues.length; j++) {
-	            var curr_issue = issues[j],
-	                issue_points = curr_issue.querySelector('span.aui-badge');
+            for (var j = 0; j < issues.length; j++) {
+                var curr_issue = issues[j],
+                    issue_points = curr_issue.querySelector('span.aui-badge');
 
-	            if (issue_points && !isNaN(parseFloat(issue_points.innerHTML))) {
-	                col_points += parseFloat(issue_points.innerHTML);
-	            }
-	        }
-	        results.push({'name': headers[i].querySelector('h2').innerHTML,'points' : col_points});
-	        headers[i].querySelector('h2 div').innerHTML = '<b>' + col_points + '</b>';
-	        total_points += col_points;
-	    }
-	    
-	    document.getElementById('jcp-board-points').innerHTML = total_points;
+                if (issue_points && !isNaN(parseFloat(issue_points.innerHTML))) {
+                    col_points += parseFloat(issue_points.innerHTML);
+                }
+            }
+            results.push({'name': headers[i].querySelector('h2').innerHTML,'points' : col_points});
+            headers[i].querySelector('h2 div').innerHTML = '<b>' + col_points + '</b>';
+            total_points += col_points;
+        }
+
+        document.getElementById('jcp-board-points').innerHTML = total_points;
     }
     catch(e) {}
 };
@@ -47,6 +48,6 @@ var config = { attributes: true, childList: true, characterData: true };
 observer.observe(target, config);
 
 document.addEventListener('DOMContentLoaded', function(){
-	document.removeEventListener('DOMContentLoaded', arguments.callee, false );
-	jiraPoints();
+    document.removeEventListener('DOMContentLoaded', arguments.callee, false );
+    jiraPoints();
 }, false );
